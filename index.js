49250732e25886e06e62ce2ec3b1ebccc1a4ecfc
tasha maina <tashabanks271@gmail.com>
2025-06-23 18:13:04 +0300

    Completed destructuring assignment

--- index.js
+++ index.js
@@ -25,24 +25,38 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
+// ----------- STRINGS ------------
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+// 1. Assign variables based on animal sounds
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+// 2. Bolt (the horse) wandered off, assign four animals
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+// 3. Chicken goes back to the coop – use color nicknames for remaining
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
 
-// Arrays
+// ----------- ARRAYS ------------
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+// 4. Destructure color names into individual variables
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+// 5. Destructure, skip indigo, use first letter variable names
+const [r, o, y, g, b, , v] = colors;
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. Only assign indigo
+const [, , , , , indg] = colors;
 
-// Objects
+// ----------- OBJECTS ------------
 
-// 7. Use destructuring to assign all variables using the keys as the variable names
+// 7. Destructure all values from muppet object
+const { muppetName, color, song, job, partner } = muppet;
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+// 8. Destructure song2 and song4 from nested album, and Kermit's job and partner
+const {
+  album: {
+    theMuppetMovie: { song2, song4 }
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
