86789949c1849a31fb79ec7476fdc9a4b8157b04
tasha maina <tashabanks271@gmail.com>
2025-06-27 23:08:03 +0300

    Completed destructuring assignment

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
 const farmAnimals = 'cow horse sheep pig chicken';
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg] = colors;
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,6 +14,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -25,34 +31,6 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// ----------- STRINGS ------------
-
-// 1. Assign variables based on animal sounds
-const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
-
-// 2. Bolt (the horse) wandered off, assign four animals
-const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
-
-// 3. Chicken goes back to the coop – use color nicknames for remaining
-const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
-
-// ----------- ARRAYS ------------
-
-// 4. Destructure color names into individual variables
-const [red, orange, yellow, green, blue, indigo, violet] = colors;
-
-// 5. Destructure, skip indigo, use first letter variable names
-const [r, o, y, g, b, , v] = colors;
-
-// 6. Only assign indigo
-const [, , , , , indg] = colors;
-
-// ----------- OBJECTS ------------
-
-// 7. Destructure all values from muppet object
-const { muppetName, color, song, job, partner } = muppet;
-
-// 8. Destructure song2 and song4 from nested album, and Kermit's job and partner
 const {
   album: {
     theMuppetMovie: { song2, song4 }
